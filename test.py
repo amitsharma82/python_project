be2aef2bf5ce1d7929f00331b86a44294425904a
Amit Sharma <amsha@Amits-MacBook-Pro.local>
2020-05-27 20:11:18 +0100

    remve the file

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import math
-import os
 
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
-
-
-print(greet('World'))
-print(greet('Corey'))
