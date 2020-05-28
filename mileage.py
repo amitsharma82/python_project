873c6844815986344e28f6c8fe8630c1940eefed
amitsharma82 <amitsharma.sharma82@gmail.com>
2020-05-28 13:50:36 +0100

    new files

---
+++ mileage.py
@@ -0,0 +1,5 @@
+print("How many Kms you run today")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"You have ran {miles} miles")
---
+++ variable.py
@@ -0,0 +1,33 @@
+x = 4, 7, 6, "amit"
+print(x)
+print(id(x))
+print(type(x))
+
+
+print("Hello \nWorld")
+print("hello \\world")
+
+print("this is 'ovely' world")
+print(f'this is lovely wrld')
+
+print("hello \ngood bye")
+
+print("/\\/\\/\\")
+
+print("this is \"new\" baoy")
+quotation = "My cat said \"meow\""
+print(quotation)
+
+greeting = "hello"
+name = "amit"
+greet_name = greeting + " " + name
+print(greet_name)
+
+first = "Amit"
+Last = "Sharma"
+formatted = "First Name: {} \nLast Name: {}".format(first, Last)
+
+print(f"First Name: {first} \nLast Name: {Last}")
+print("First Name: {}, Last Name {}".format(first, Last))
+print(formatted)
+
