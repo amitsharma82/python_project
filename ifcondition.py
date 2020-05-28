7ffca264e9bc0cc27498dd9cacef2e373d456d9f
amitsharma82 <amitsharma.sharma82@gmail.com>
2020-05-28 15:10:13 +0100

    ll

---
+++ ifcondition.py
@@ -0,0 +1,10 @@
+name = input("Enter your full Name: ")
+
+if name == "Amit Sharma":
+    print(f"{name} your are welcome in the house")
+
+elif name == "Sam Sharma":
+    print(f"{name} you need to call before coming")
+
+else:
+    print(f"{name} you are not welcome in the house")
